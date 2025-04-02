793e41436a573ec17ac90583f0bd19823e0e6c19
pratikshinde590 <pratik.shindee590@gmail.com>
2025-04-02 19:16:35 +0530

    build: Update AWS & Azure cluster infra []

--- AWS/module/terraform.tfvars
+++ AWS/module/terraform.tfvars
@@ -11,10 +11,6 @@ eks_node_group1_desired_size  = 1
 eks_node_group1_instance_type = ["t3.medium"]
 eks_node_group1_max_size      = 2
 eks_node_group1_min_size      = 1
-# eks_node_group2_desired_size  = 2
-# eks_node_group2_instance_type = ["t3.large"]
-# eks_node_group2_max_size      = 2
-# eks_node_group2_min_size      = 1
 eks_node_group_ami_type       = "AL2_x86_64"
 enable_dns_hostnames          = true
 enable_nat_gateway            = true
@@ -26,16 +22,4 @@ single_nat_gateway            = true
 vpc_cidr                      = "10.0.0.0/16"
 vpc_name                      = "sparrow-vpc"
 
-# eks_cluster_name_2              = "sparrow-eks-2"
-# eks_cluster_version_2           = "1.32"
-# eks_node_group1_desired_size_2  = 2
-# eks_node_group1_instance_type_2 = ["t3.medium"]
-# eks_node_group1_max_size_2      = 3
-# eks_node_group1_min_size_2      = 1
-# eks_node_group2_desired_size_2  = 2
-# eks_node_group2_instance_type_2 = ["t3.large"]
-# eks_node_group2_max_size_2      = 2
-# eks_node_group2_min_size_2      = 1
-# eks_node_group_ami_type_2       = "AL2_x86_64"
-# irsa_role_name_2                = "AmazonEKSTFEBSCSIRole-2"
 
--- AWS/resources/eks/variables.tf
+++ AWS/resources/eks/variables.tf
@@ -40,26 +40,6 @@ variable "eks_node_group1_min_size" {
   type        = number
 }
 
-# variable "eks_node_group2_desired_size" {
-#   description = "Desired size for EKS node group 2"
-#   type        = number
-# }
-
-# variable "eks_node_group2_instance_type" {
-#   description = "Instance type for EKS node group 2"
-#   type        = list(string)
-# }
-
-# variable "eks_node_group2_max_size" {
-#   description = "Max size for EKS node group 2"
-#   type        = number
-# }
-
-# variable "eks_node_group2_min_size" {
-#   description = "Min size for EKS node group 2"
-#   type        = number
-# }
-
 variable "eks_node_group_ami_type" {
   description = "AMI type for EKS node groups"
   type        = string
@@ -109,75 +89,3 @@ variable "vpc_name" {
   description = "Name of the VPC"
   type        = string
 }
-
-# variable "eks_cluster_name_2" {
-#   description = "EKS cluster name for the second cluster"
-#   type        = string
-#   default     = "infovision-eks-2"
-# }
-
-# variable "eks_cluster_version_2" {
-#   description = "EKS cluster version for the second cluster"
-#   type        = string
-#   default     = "1.27"
-# }
-
-# variable "eks_node_group1_desired_size_2" {
-#   description = "Desired size of node group 1 for the second cluster"
-#   type        = number
-#   default     = 2
-# }
-
-# variable "eks_node_group1_instance_type_2" {
-#   description = "Instance type for node group 1 in the second cluster"
-#   type        = list(string)
-#   default     = ["t3.small"]
-# }
-
-# variable "eks_node_group1_max_size_2" {
-#   description = "Maximum size of node group 1 for the second cluster"
-#   type        = number
-#   default     = 3
-# }
-
-# variable "eks_node_group1_min_size_2" {
-#   description = "Minimum size of node group 1 for the second cluster"
-#   type        = number
-#   default     = 1
-# }
-
-# variable "eks_node_group2_desired_size_2" {
-#   description = "Desired size of node group 2 for the second cluster"
-#   type        = number
-#   default     = 2
-# }
-
-# variable "eks_node_group2_instance_type_2" {
-#   description = "Instance type for node group 2 in the second cluster"
-#   type        = list(string)
-#   default     = ["t3.small"]
-# }
-
-# variable "eks_node_group2_max_size_2" {
-#   description = "Maximum size of node group 2 for the second cluster"
-#   type        = number
-#   default     = 2
-# }
-
-# variable "eks_node_group2_min_size_2" {
-#   description = "Minimum size of node group 2 for the second cluster"
-#   type        = number
-#   default     = 1
-# }
-
-# variable "eks_node_group_ami_type_2" {
-#   description = "AMI type for the second cluster"
-#   type        = string
-#   default     = "AL2_x86_64"
-# }
-
-# variable "irsa_role_name_2" {
-#   description = "IRSA role name for the second cluster"
-#   type        = string
-#   default     = "AmazonEKSTFEBSCSIRole-2"
-# }
